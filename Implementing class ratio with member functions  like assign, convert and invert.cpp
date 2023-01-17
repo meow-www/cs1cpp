ed88464a3b6f294e78475afeb4b50058634a53d9
meow-www <122848274+meow-www@users.noreply.github.com>
2023-01-17 08:34:13 +0530

    Create Implementing class ratio with member functions  like assign, convert and invert.cpp

---
+++ Implementing class ratio with member functions  like assign, convert and invert.cpp
@@ -0,0 +1,46 @@
+#include <iostream.h>
+#include <conio.h>
+class ratio
+{
+public:
+    Void assign(int, int);
+    Double convert();
+    void invert();
+    void print();
+
+private:
+    int num, den;
+};
+void main()
+{
+    ratio x;
+    x.assign(22, 7);
+    clrscr();
+    cout << "x=";
+    x.print();
+    cout << "=" << x.convert() << "\n";
+    x.invert();
+    cout << "1/x=";
+    x.print();
+    cout << "\n";
+    getch();
+}
+void ratio::assign(int numerator, int denominator)
+{
+    num = numerator;
+    den = denominator;
+}
+double ratio::convert()
+{
+    return (double)num / den;
+}
+void ratio::invert()
+{
+    int temp = num;
+    num = den;
+    den = temp;
+}
+void ratio::print()
+{
+    cout << num << "/" << den;
+}
