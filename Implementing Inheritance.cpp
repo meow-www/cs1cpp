8dfe0fd19cd339c68aaf31c344cd2d3a7feca67f
meow-www <122848274+meow-www@users.noreply.github.com>
2023-01-17 08:37:25 +0530

    Create Implementing Inheritance.cpp

---
+++ Implementing Inheritance.cpp
@@ -0,0 +1,85 @@
+#include <iostream.h>
+#include <conio.h>
+class student
+{
+protected:
+    int roll;
+    char name[20];
+
+public:
+    void getinfo()
+    {
+        cout << "Enter rollno and
+            name : "; cin>>roll>>name;
+    }
+    void showinfo()
+    {
+        cout << "Roll No :" << roll << endl;
+        cout << "Name :" << name << endl;
+    }
+};
+class test : public student
+{
+protected:
+    int m1, m2;
+
+public:
+    void getmarks()
+    {
+        cout << "Enter marks of Subject 1:";
+        cin >> m1;
+        cout << "Enter marks of Subject 2:";
+        cin >> m2;
+    }
+    void showmarks()
+    {
+        cout << "Subject1 Marks:" << m1 << endl;
+        cout << "Subject 2 Marks :" << m2 << endl;
+    }
+};
+class sports
+{
+protected:
+    int spmarks;
+
+public:
+    void getsports()
+    {
+        cout << "Enter Sports marks :";
+        cin >> spmarks;
+    }
+    void showsports()
+    {
+        cout << "Sports Marks :" << spmarks << endl;
+    }
+};
+class result : public test, public sports
+{
+    int total, per;
+
+public:
+    void calculate()
+    {
+        total = m1 + m2 + spmarks;
+        per = total / 3;
+    }
+    void showresult()
+    {
+        cout << "Total Marks :" << total << endl;
+        cout << "Percentage :" << per;
+    }
+};
+void main()
+{
+    clrscr();
+    result s;
+    s.getinfo();
+    s.getmarks();
+    s.getsports();
+    s.calculate();
+    s.showinfo();
+    s.showmarks();
+    s.showsports();
+    s.showresult();
+    getch();
+}
