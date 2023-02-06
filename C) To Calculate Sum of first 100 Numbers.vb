e3e2f3604a406e2fb06629d515123e3e209ac734
meow-www <122848274+meow-www@users.noreply.github.com>
2023-02-07 02:16:31 +0530

    Update and rename To Calculate Sum of first 100 Numbers.vb to C) To Calculate Sum of first 100 Numbers.vb

---
+++ C) To Calculate Sum of first 100 Numbers.vb
@@ -0,0 +1,25 @@
+Private Sub Command1_Click()
+  Dim i As Integer, sum As Integer
+  sum = 0
+  i = 1
+Do
+  List1.AddItem i
+  sum = sum + i
+  i = i + 1
+  Loop While (i <= 100)
+  Label1.Caption = "Sum of 1..100 numbers :" &
+sum End Sub
+  
+/////////////////
+  
+Private Sub Command1_Click()
+Dim i As Integer, sum As Integer
+sum =0
+i= 1
+Do
+List1.AddItem i
+sum = sum + i
+i = i+1
+Loop While (i<=100)
+Label1.Caption= "sum of 1...100 Numbers: ",&sum
+End Sub
